773f45e522c18a22f5d1232ba7cc78a756a23245
Jirald Calusay <jirald.calusay@awsys-i.com>
2026-06-12 05:07:01 +0800

    Fix PWA cache refresh for deployments

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kwarta-v2";
+const CACHE_NAME = "kwarta-v3";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/icons/icon-192.png"];
 
 self.addEventListener("install", (event) => {
@@ -56,12 +56,8 @@ self.addEventListener("fetch", (event) => {
   }
 
   event.respondWith(
-    caches.match(request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      return fetch(request).then((response) => {
+    fetch(request)
+      .then((response) => {
         if (!response || response.status !== 200) {
           return response;
         }
@@ -69,7 +65,7 @@ self.addEventListener("fetch", (event) => {
         const copy = response.clone();
         caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
         return response;
-      });
-    }),
+      })
+      .catch(() => caches.match(request) || Response.error()),
   );
 });
